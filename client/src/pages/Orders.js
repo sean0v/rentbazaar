ed9482369a4a5d76d69e0c6e07292a15c4949e0a
brajgor <artemij.burovs@gmail.com>
2025-06-15 23:04:33 +0300

    fix

--- client/src/pages/Orders.js
+++ client/src/pages/Orders.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useAuth } from "../context/AuthContext";
-import { format } from "date-fns"; 
+import { format } from "date-fns";
 import { Link } from "react-router-dom";
 
 const ORDER_STATUSES = {
@@ -20,7 +20,7 @@ const Orders = () => {
 
   useEffect(() => {
     if (!userId) return;
-    
+
     fetch(`https://rentbazaar-app.azurewebsites.net/api/orders/getOrders/${userId}`)
       .then((res) => res.json())
       .then((data) => setOrders(data.orders || []))
@@ -43,14 +43,14 @@ const Orders = () => {
         method: "POST",
         headers: { "Content-Type": "application/json" }
       });
-  
+
       if (!response.ok) throw new Error("Error updating");
-  
+
       window.location.reload();
     } catch (error) {
       console.error("Error", error);
     }
-}
+  }
 
   return (
     <div className="container mt-4">
@@ -58,45 +58,47 @@ const Orders = () => {
 
       <div className="row">
         {orders.length > 0 ? (
-          orders.map((order) => (
-            <div key={order.id} className="col-12 mb-3">
-              <div className="card shadow-sm">
-                <div className="card-body d-flex align-items-center justify-content-between">
-                  <div>
-                    <h5 className="card-title">Pasūtījuma #{order.id}</h5>
-                    <p className="card-text">
-                        
-                      <strong>Status:</strong> {ORDER_STATUSES[order.status] || "Unknown status"}
-                    </p>
-                    <p className="card-text">
-                      <strong>Datums:</strong> {format(new Date(order.createdAt), "dd.MM.yyyy")}
-                    </p>
-                    <button
-                      className="btn btn-link p-0"
-                      onClick={() => toggleOrderDetails(order.id)}
-                    >
-                      {expandedOrderId === order.id ? "Aizvērt detaļas" : "Parādīt detaļas"}
-                    </button>
+          orders.map((order) => { return (
+              <div key={order.id} className="col-12 mb-3">
+                <div className="card shadow-sm">
+                  <div className="card-body d-flex align-items-center justify-content-between">
+                    <div>
+                      <h5 className="card-title">Pasūtījuma #{order.id}</h5>
+                      <p className="card-text">
+
+                        <strong>Status:</strong> {ORDER_STATUSES[order.status] || "Unknown status"}
+                      </p>
+                      <p className="card-text">
+                        <strong>Datums:</strong> {format(new Date(order.createdAt), "dd.MM.yyyy")}
+                      </p>
+                      <button
+                        className="btn btn-link p-0"
+                        onClick={() => toggleOrderDetails(order.id)}
+                      >
+                        {expandedOrderId === order.id ? "Aizvērt detaļas" : "Parādīt detaļas"}
+                      </button>
+                    </div>
+
+                    <div className="d-flex">
+                      {order.status === 2 ? null : <button className="btn btn-success me-2" onClick={() => approveOrder(order.id)}>Apstiprināt</button>}
+                      <Link to={`/review/${order.offer.id}`} className="btn btn-primary">Atsauces</Link>
+                    </div>
                   </div>
 
-                  <div className="d-flex">
-                    {order.status === 2 ? null : <button className="btn btn-success me-2" onClick={() => approveOrder(order.id)}>Apstiprināt</button>}
-                    <Link to={`/review/${order.offer.id}`} className="btn btn-primary">Atsauces</Link>
-                  </div>
+                  {expandedOrderId === order.id && order.offer && (
+                    <div className="card-footer bg-light">
+                      <h6 className="mb-2">{order.offer.name}</h6>
+                      <p className="mb-1"><strong>Kategorija:</strong> {OFFER_TYPES[order.offer.type]}</p>
+                      <p className="mb-1"><strong>Cena par produktu:</strong> {order.offer.price} €</p>
+                      <p className="mb-1"><strong>Daudzums:</strong> {order.quantity}</p>
+                      <p className="mb-1"><strong>Cena kopā:</strong> {Number(order?.offer?.price) * Number(order?.quantity)} €</p>
+                      <p className="mb-1"><strong>Apraksts:</strong> {order.offer.description}</p>
+                    </div>
+                  )}
                 </div>
-
-                {expandedOrderId === order.id && order.offer && (
-                  <div className="card-footer bg-light">
-                    <h6 className="mb-2">{order.offer.name}</h6>
-                    <p className="mb-1"><strong>Kategorija:</strong> {OFFER_TYPES[order.offer.type]}</p>
-                    <p className="mb-1"><strong>Cena par Produktu:</strong> {order.offer.price} €</p>
-                    <p className="mb-1"><strong>Cena kopā:</strong> {Number(order?.offer?.price) * Number(order?.offer?.quantity)} €</p>
-                    <p className="mb-1"><strong>Apraksts:</strong> {order.offer.description}</p>
-                  </div>
-                )}
               </div>
-            </div>
-          ))
+            )
+          })
         ) : (
           <p className="text-center text-muted">Vēl nav pasūtījumu</p>
         )}
